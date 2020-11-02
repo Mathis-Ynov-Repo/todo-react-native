9d8327fd797442f6a574340350117cd5e749ad8d
EyZex <eyzex@hotmail.fr>
2020-11-02 17:47:52 +0100

    added asyncStorage

--- App.js
+++ App.js
@@ -1,131 +1,151 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import { StyleSheet, Text, View, ScrollView, TouchableOpacity, TextInput, SafeAreaView, Button, Image } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      list: [],
-      text: '',
-    };
+const STORAGE_KEY = 'TASKS'
+const App = () => {
+  const [list, setList] = useState([]);
+  const [text, setText] = useState('')
+  saveData = async (tmpList) => {
+    try {
+      await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(tmpList))
+      // alert('Data successfully saved')
+    } catch (e) {
+      console.log(e)
+      alert('Failed to save the data to the storage')
+    }
   }
+  readData = async () => {
+    try {
+      const list = await AsyncStorage.getItem(STORAGE_KEY)
+      if (list !== null) {
+        setList(JSON.parse(list))
+      }
+    } catch (e) {
+      alert('Failed to fetch the data from storage')
+    }
+  }
+  const clearStorage = async () => {
+    try {
+      await AsyncStorage.clear()
+      alert('Storage successfully cleared!')
+    } catch (e) {
+      alert('Failed to clear the async storage.')
+    }
+  }
+  useEffect(() => {
+    readData()
+  }, [])
 
   ListItem = ({ value, index }) => {
     let state = value.state;
     return (
       <View>
-        <TouchableOpacity style={[styles.item, state == true ? styles.success : styles.todo]} 
-        onPress={() => this.changeState(index)}
+        <TouchableOpacity style={[styles.item, state == true ? styles.success : styles.todo]}
+          onPress={() => changeState(index)}
         >
-          <Text style={{fontSize: 18, flex: 0.7}}> 
-            {value.text} 
-          </Text> 
+          <Text style={{ fontSize: 18, flex: 0.7 }}>
+            {value.text}
+          </Text>
           <TouchableOpacity
-          style={styles.button}
-          onPress={() => this.deleteItem(index)}
+            style={styles.button}
+            onPress={() => deleteItem(value)}
           >
-            <Text style={{color: 'red', margin: 5}}>Delete</Text>
+            <Text style={{ color: 'red', margin: 5 }}>Delete</Text>
           </TouchableOpacity>
         </TouchableOpacity>
-        
+
       </View>
     );
   };
 
   NavBar = () => (
-    <View style={{ height: 60, backgroundColor: 'white', justifyContent: 'center',alignItems:'center', marginBottom: 5 }}>
+    <View style={{ height: 60, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center', marginBottom: 5 }}>
       <Text style={{ fontWeight: 'bold', fontSize: 20, marginTop: 10, marginLeft: 15 }}>My TodoList</Text>
     </View>
   );
 
-  deleteItem = (index) => {
-    this.state.list.splice(index, 1);
-    this.setState(
-      {list: this.state.list}
-    )
+  deleteItem = (item) => {
+    // could have use splice aswell
+    let tmp = list.filter((val) => val != item)
+    saveData(tmp);
+    setList(tmp);
   };
   changeState = (index) => {
-    this.state.list.map((value, i) => {
-      index != i ? value : value.state = !value.state
+    let tmp = list.map((value, i) => {
+      if (index != i) {
+        return value
+      } else {
+        value.state = !value.state;
+        return value
+      }
     })
-    this.setState(
-      {list: this.state.list}
-    )
+    saveData(tmp);
+    setList(tmp);
+
   }
-  // used to have a done btn
-  // addSuccess = (index) => {
-  //   this.state.list.map((value, i) => {
-  //     index != i ? value : value.state = true
-  //   })
-  //   this.setState(
-  //     {list: this.state.list}
-  //   )
-  // }
 
   addItem = () => {
     //ES5
-    // this.state.list.push(this.state.text)
+    // list.push(this.state.text)
 
-    if (this.state.text != '') {
-    //ES6
-      this.setState(
-        {
-          list: [...this.state.list, {text : this.state.text, state: false}],
-          text: ''
-        },
-      ),
-      this.textInput.clear()
+    if (text != '') {
+      //ES6
+      let tmp = [...list, { text: text, state: false }]
+      saveData(tmp);
+      setList(tmp)
+      setText('')
+      textInput.clear()
     }
   };
 
-  render() {
-    return (
-      <SafeAreaView style={{ flex: 1, backgroundColor: '#efefef' }}>
-        <this.NavBar />
-        {this.state.list.length > 0 ? (
-          <ScrollView>
-            {this.state.list.map((value, index) => {
-              return <this.ListItem value={value} index={index} key={index}/>
-            })
-             
-            }
-            
-          </ScrollView>
-        ) : (
-          <ScrollView contentContainerStyle={{alignItems: "center", justifyContent: "center", flexGrow: 1}}>
-            
-            <Text style={{fontSize: 24}}>No items in the list yet !</Text>
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#efefef' }}>
+      <NavBar />
+      {list.length > 0 ? (
+        <ScrollView>
+          {list.map((value, index) => {
+            return <ListItem value={value} index={index} key={index} />
+          })
+
+          }
+
+        </ScrollView>
+      ) : (
+          <ScrollView contentContainerStyle={{ alignItems: "center", justifyContent: "center", flexGrow: 1 }}>
+
+            <Text style={{ fontSize: 24 }}>No items in the list yet !</Text>
             <Image
-            source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}
+              source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
             />
 
           </ScrollView>
-          
-        )}
-        <View>
-            <>
-              <TextInput
-              style={styles.input}
-              placeholder="Type your new task here!"
-              maxLength={40}
-              onChangeText={text => this.setState({text: text})}
-              ref={input => { this.textInput = input }}
-              />
-              <Button
-              style={styles.button}
-              title="Add"
-              onPress={this.addItem}
-              />
-            </>
 
-        </View>
-        <StatusBar style="auto" />
-      </SafeAreaView>
-    );
-  }
+        )}
+      <View>
+        <>
+          <TextInput
+            style={styles.input}
+            placeholder="Type your new task here!"
+            maxLength={40}
+            onChangeText={text => setText(text)}
+            ref={input => { textInput = input }}
+          />
+          <Button
+            style={styles.button}
+            title="Add"
+            onPress={addItem}
+          />
+        </>
+
+      </View>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
 }
+
 
 const styles = StyleSheet.create({
 
@@ -163,4 +183,4 @@ const styles = StyleSheet.create({
     textAlign: "center"
   },
 });
-
+export default App;
